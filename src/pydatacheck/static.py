02746df3ddec5293e3161a90cfb547b18f53033a
Mark Veltzer <mark.veltzer@gmail.com>
2026-09-05 11:17:31 +0300

    no commit message given

--- src/pydatacheck/static.py
+++ src/pydatacheck/static.py
@@ -1,5 +1,5 @@
 """ version which can be consumed from within the module """
-VERSION_STR = "0.0.10"
+VERSION_STR = "0.0.11"
 DESCRIPTION = "Pydatacheck checks yaml data files"
 APP_NAME = "pydatacheck"
 LOGGER_NAME = "pydatacheck"
